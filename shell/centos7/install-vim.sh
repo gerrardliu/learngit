cc06bd084c96fead8bab9eecea7da97001d8699b
yan.liu <yan.liu@bitmain.com>
2018-11-01 16:58:20 +0800

    fix problems in install-vim.sh

--- shell/centos7/install-vim.sh
+++ shell/centos7/install-vim.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo yum install ncurses-devel python-devel
+sudo yum install -y gcc gcc-c++ ncurses-devel python-devel
 git clone https://github.com/vim/vim.git
 cd vim
 git pull
-./configure --with-features=huge --enable-pythoninterp --with-python-config-dir=/usr/lib64/python2.7/config --enable-gui=gtk2 --enable-cscope --enable-luainterp --enable-multibyte
-make
-sudo make install
+./configure --with-features=huge --enable-pythoninterp --with-python-config-dir=/usr/lib64/python2.7/config --enable-gui=gtk2 --enable-cscope --enable-luainterp --enable-multibyte \
+&& make \
+&& sudo make install \
+&& sudo rm -rf vim
