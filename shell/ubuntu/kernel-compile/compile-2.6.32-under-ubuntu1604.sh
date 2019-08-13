616cebae7bd6cc4f99a8a4e9ba6180deaf9eba80
gerrardliu <yard1980@gmail.com>
2019-08-13 18:42:30 +0800

    update

--- shell/ubuntu/kernel-compile/compile-2.6.32-under-ubuntu1604.sh
+++ shell/ubuntu/kernel-compile/compile-2.6.32-under-ubuntu1604.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 #1. down grade gcc5 to gcc4.8
+apt-get install gcc-4.8
+ls /usr/bin/gcc*
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 100
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 90
+update-alternatives --config gcc
+gcc -v
 #2. Makefile -m64 -m32
+vim arch/x86/vdso/Makefile
 #3. !define(@val) to !@val
+vim kernel/timeconst.pl +373
 #4. comment 2nd *page
+vim drivers/net/igbvf/igbvf.h +128
