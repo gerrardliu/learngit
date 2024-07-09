5f189425e3609ba225e9646488dade7c68e117bf
yan.liu <yan.liu@newche.com>
2024-07-09 11:26:45 +0800

    update

--- shell/centos7/install-ceph.sh
+++ shell/centos7/install-ceph.sh
@@ -1,44 +1,58 @@
+# set static ip
 cd /etc/sysconfig/network-scripts/
 vi ifcfg-ens33
 service network restart
 
+# change yum source
 curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 yum clean all
 yum makecache
 
+#install sshd
 yum install -y openssh-server
 systemctl start sshd
 systemctl enable sshd
 
+#install tools, ifconfig
 yum install net-tools
 
+#set hostname on every node
 hostnamectl set-hostname ceph1
 hostnamectl status
 
+#disable fireware
 setenforce 0
 vi /etc/selinux/config
 systemctl disable --now firewalld
 
+#set ssh password free
 ssh-keygen -f /root/.ssh/id_rsa -P ''
 ssh-copy-id -o StrictHostKeyChecking=no 192.168.78.11
 ssh-copy-id -o StrictHostKeyChecking=no 192.168.78.12
 ssh-copy-id -o StrictHostKeyChecking=no 192.168.78.13
 
+#set /etc/hosts
 vi hosts
 scp hosts root@192.168.78.12:/etc/
 scp hosts root@192.168.78.13:/etc/
 
+#time sync
 timedatectl set-timezone Asia/Shanghai
 timedatectl status
+yum install -y ntpdate
+ntpdate -u ntp1.aliyun.com
 
+#install docker
 yum install -y yum-utils
 yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 yum install -y docker-ce
 systemctl start docker
 systemctl enable docker
 
+#install python
 yum install -y python3
 
+#install cephadm
 curl --silent --remote-name --location https://mirrors.chenby.cn/https://github.com/ceph/ceph/raw/octopus/src/cephadm/cephadm
 chmod 755 cephadm
 ./cephadm add-repo --release octopus
@@ -59,16 +73,32 @@ ceph -s
 ssh-copy-id -f -i /etc/ceph/ceph.pub root@ceph2
 ssh-copy-id -f -i /etc/ceph/ceph.pub root@ceph3
 
+#add host
 ceph orch host add ceph2
 ceph orch host add ceph3
 ceph orch host ls
 
+#add mon
 ceph orch apply mon --placement="3 ceph1 ceph2 ceph3"
+
+#add mgr
 ceph orch apply mgr --placement="3 ceph1 ceph2 ceph3"
 
+#add osd
 ceph orch daemon add osd ceph1:/dev/sdb
 ceph orch device ls
 ceph orch daemon add osd ceph1:/dev/sdc
 
+#create rgw
+radosgw-admin realm create --rgw-realm=realm1 --default
+radosgw-admin zonegroup create --rgw-zonegroup=zonegroup1 --master --default
+radosgw-admin zone create --rgw-zonegroup=zonegroup1 --rgw-zone=zone1 --master --default
+ceph orch apply rgw realm1 zone1 --placement="3 ceph1 ceph2 ceph3"
 
-
+#enable rgw object getate functions in dashboard
+radosgw-admin user create --uid=dashboard --display-name=dashboard --system
+radosgw-admin user info --uid=dashboard | grep secret_key
+echo PXU8AUKZA6P4UCFL3W1V >  /root/rgw-dashboard-access-key
+echo llwxd5oumcNZT3B0eObXBYXMiZsIuhtJRiYUcO4X > /root/rgw-dashboard-secret-key
+ceph dashboard set-rgw-api-access-key -i /root/rgw-dashboard-access-key
+ceph dashboard set-rgw-api-secret-key -i /root/rgw-dashboard-secret-key
